be5bba865c805e46aa243d6decb0de35154c6e8a
Nasseima <lindsaynasseima@gmail.com>
2024-06-10 11:47:01 -0400

    finished my lab

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 //Part 1
 //Select main element and name it mainEl
 const mainEl = document.querySelector('main');
-//Set it to this a specific background color
-mainEl.style.background = 'var(--main-bg)'
+
 console.log (mainEl)
+//Set it to this a specific background color
+mainEl.style.backgroundColor = 'var(--main-bg)'
+
 //Create a h1 element
 let h1 = document.createElement("h1");
-console.log(h1);
 //Add text to the h1 element
-const hText = document.createTextNode('DOM MANIPULATION')
-h1.innerHTML = "DOM MANIPULATION"
-mainEl.appendChild(hText);
-// console.log(hText)
-// const text = document.querySelector("h1").innerText = "DOM MANIPULATION";
-// console.log(text)
+h1.innerHTML = "DOM MANIPULATION";
+
+mainEl.appendChild(h1);
+//Add class of flex
+mainEl.classList.add('flex-ctr')
+
+//Part Two: Creating a Menu Bar
+
+const topMenuEl = document.querySelector("#top-menu")
+
+//Set height to 100%
+
+topMenuEl.style.height = "100%"
+
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+topMenuEl.classList.add("flex-around");
+
+// Part Three: Adding menu buttons
+
+const menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
+menuLinks.forEach((link)=>{
+
+    //create anchor element
+    const aElement = document.createElement("a");
+    //added href
+    aElement.href = link.href
+    //added text content
+    aElement.textContent = link.text
+    //appended to topmenu element
+    topMenuEl.appendChild(aElement);
+})
